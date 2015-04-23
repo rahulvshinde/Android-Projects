b6504d221316f8312bfbc8b38f35390aa7f5eed1
rahulvshinde <rshinde@mail.csuchico.edu>
2015-04-23 02:00:12 -0700

    Project Copied

---
+++ DoctorAssistant/app/src/main/java/android_project/com/doctorassistant/DoctorLogin.java
@@ -0,0 +1,16 @@
+package android_project.com.doctorassistant;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by RAHUL on 4/23/2015.
+ */
+public class DoctorLogin extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.doctorlogin);
+    }
+}
+
